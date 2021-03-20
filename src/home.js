9507b3ecd2f2757be12b53d9e5f01ddb64107d43
Tyler Cheney <tyler.cheney.05@gmail.com>
2021-03-20 12:50:19 -0600

    home page

--- src/home.js
+++ src/home.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Navbar from './components/grocery/Navbar';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Navbar />
-    <App />
+    <h1>Home Page</h1>
   </React.StrictMode>,
-  document.getElementById('app')
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
